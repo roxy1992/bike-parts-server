641a4d77c12b7c23e2f71e05d5607ce92ce32088
roxy1992 <roxybhuiyan1992@gmail.com>
2022-05-23 13:05:20 +0600

    cunnect with local host5000

--- index.js
+++ index.js
@@ -14,13 +14,31 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkitf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('Bike parts DB cunnected')
-    // perform actions on the collection object
-    client.close();
-});
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('Bike parts DB cunnected')
+//     // perform actions on the collection object
+//     client.close();
+// });
 
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('bikeParts').collection('parts');
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('running Biks Parts Manufacturer')
